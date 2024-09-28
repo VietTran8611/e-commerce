3a0b2936648b117940d8493d2ab8e6447c86f86c
Omnilios <omnilios123@gmail.com>
2024-09-28 00:13:21 -0400

    add lower nav

--- src/components/Nav/LowerNav.jsx
+++ src/components/Nav/LowerNav.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 export const LowerNav = () => {
   return (
-    <ul className='upperNav'>
-        Lower
+    <ul className='upperNav lowerNav'>
+        <div className='logo'>
+          <p className='bold'>Company Name</p>
+        </div>
+        <div className='navUltility'>
+          <div className='search-container'>
+            <input type="text" placeholder='Search...' />
+            <img src="/e-commerce/img/search.png" alt="" />
+          </div>
+          <a className='cart-con' href="#">
+            <img className='cart' src="/e-commerce/img/cart.png" alt="" />
+          </a>
+        </div>
     </ul>
   )
 }
